1fa860127806046ff2ef2a92b94f33bdc4a55dd8
Milky2018 <842376130@qq.com>
2024-05-21 15:53:31 +0800

    Add an example for stream definition.

--- src/Ty.hs
+++ src/Ty.hs
@@ -8,6 +8,7 @@ data LType =
     LTLam LType LType
   | LTId String 
   | LTAll String LType 
+  | LTVar String 
 
 instance Eq LType where 
   LTLam t1 t2 == LTLam t1' t2' = t1 == t1' && t2 == t2'
@@ -19,6 +20,9 @@ instance Eq LType where
       subst (LTId name) name' t = if name == name' then LTId t else LTId name
       subst (LTLam t1 t2) name' t = LTLam (subst t1 name' t) (subst t2 name' t)
       subst (LTAll name'' t) name' t' = if name'' == name' then LTAll name'' t else LTAll name'' (subst t name' t')
+      subst (LTVar v) _ _ = error $ "There should not be type variable " ++ v
+  LTVar v == _ = error $ "There should not be type variable " ++ v
+  _ == LTVar v = error $ "There should not be type variable " ++ v
   _ == _ = False
 
 type UDT = [String]
@@ -27,4 +31,4 @@ instance Pretty LType where
   pretty (LTLam t1 t2) = parens $ pretty t1 <+> pretty "->" <+> pretty t2
   pretty (LTId name) = pretty name
   pretty (LTAll name t) = parens $ angles (pretty name) <+> pretty t
-
+  pretty (LTVar name) = pretty name
